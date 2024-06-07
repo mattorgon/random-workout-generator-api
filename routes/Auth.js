87b4b3b5b84b84570f2b160ac62c8712dd24dfce
Matt Orgon <matthewaorgon@gmail.com>
2024-06-07 16:19:42 -0700

    bcryptjs change

--- routes/Auth.js
+++ routes/Auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // Register a new user
 // router.post("/register", async (req, res) => {
