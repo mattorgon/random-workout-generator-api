4ed9e3271507493a1cbf7f8ce4f383043a137a4c
Matt Orgon <matthewaorgon@gmail.com>
2024-06-07 15:28:53 -0700

    Deploy combined frontend and backend

--- config/mongo.js
+++ config/mongo.js
@@ -34,7 +34,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/workoutsDB", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
